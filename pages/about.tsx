f6cf516c27c20675f1802db08f92cfeaf1b02a59
fahimeshti <fahimesti@gmail.com>
2023-03-03 02:59:37 +0600

    added About page

---
+++ pages/about.tsx
@@ -0,0 +1,27 @@
+import Footer from '@/components/Footer';
+import SponsorSection from '@/components/SponsorSection';
+import Topbar from '@/components/Topbar';
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <Topbar />
+            <section className='w-full flex r justify-center'>
+                <div className='w-full max-w-4xl pt-32 pb-20 space-y-8'>
+                    <h1 className='text-[48px] font-bold leading-none'>About Finsweet <br /> Podcast</h1>
+                    <p className='max-w-[480px] text-base'>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                    </p>
+                    <button className='bg-[#503AE7] text-white px-8 py-3'>
+                        Subscribe Now!
+                    </button>
+                </div>
+            </section>
+            <SponsorSection />
+            <Footer />
+        </div>
+    )
+}
+
+export default About;
